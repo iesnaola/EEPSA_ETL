7d14a9f9af5b40edc2f04cfa804a88dbf98545dd
Iker Esnaola <iker.esnaola@tekniker.es>
2019-01-28 10:52:18 +0100

    Extract Euskalmet Weather Stations from a URL containing a list of Weather Station XML URLs. Generate a sensor's observed quality from the sensor type ID

--- eepsa.etl.extract/src/main/java/etl/extract/euskalmet/EuskalmetWeatherStationExtractor.java
+++ eepsa.etl.extract/src/main/java/etl/extract/euskalmet/EuskalmetWeatherStationExtractor.java
@@ -2,6 +2,8 @@ package etl.extract.euskalmet;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,17 +20,59 @@ import org.xml.sax.SAXException;
 import etl.entity.Coordinates;
 import etl.entity.Sensor;
 import etl.entity.WeatherStation;
+import etl.semanticResources.Namespaces;
 
 public class EuskalmetWeatherStationExtractor {
 
-	
 	private static Log log = LogFactory.getLog(EuskalmetWeatherStationExtractor.class);
 	
 	//Constants
 	public static String baseURI = "http://www.tekniker.es/euskalmetWeatherStations#";
-	public static String weatherStationBase = "weatherStation_Euskalmet_";
-	public static String weatherStationOwnerURI = "http://es.dbpedia.org/page/Euskalmet";
+	public static String euskalmetWeatherStationBase = "weatherStation_Euskalmet_";
+	public static String euskalmetWeatherStationOwnerURI = "http://es.dbpedia.org/page/Euskalmet";
+	public static String euskalmetWeatherStationListXML = "http://opendata.euskadi.eus/contenidos/ds_meteorologicos/estaciones_meteorologicas/opendata/estaciones.xml";
 	
+	public static WeatherStation createEuskalmetWeatherStationFromXMLURL(String urlString){
+		log.debug("createEuskalmetWeatherStationFromXMLURL START");
+		
+		if(urlString.isEmpty()){
+			log.error("ERROR: in createEuskalmetWeatherStationFromXMLURL");
+			log.error("ERROR DESC: createEuskalmetWeatherStationFromXMLURL " + urlString + " IS EMPTY");
+		}
+		else
+			log.debug("createEuskalmetWeatherStationFromXMLURL urlString IS NOT EMPTY");
+			
+		WeatherStation ws = new WeatherStation();
+		
+		//Create a Document Builder
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder;
+		
+		try {
+			builder = factory.newDocumentBuilder();
+			
+			//Create a Document from a file
+			try {
+				Document doc = builder.parse(new URL(urlString).openStream());
+				ws = createEuskalmetWeatherStationFromXMLDoc(doc);
+				
+			} catch (SAXException e) {
+				log.error("ERROR: in readXMLURL");
+				log.error("ERROR DESC: SAXException. " + e);
+			} catch (IOException e) {
+				log.error("ERROR: in readXMLURL");
+				log.error("ERROR DESC: IOException. " + e);
+			}
+		}
+		 catch (ParserConfigurationException e1) {
+				log.error("ERROR: in readXMLFile");
+				log.error("ERROR DESC: Error ", e1);
+				return ws;
+		 }
+		
+		log.debug("createEuskalmetWeatherStationFromXMLURL END");
+		return ws;
+	}
 	
 	public static WeatherStation createEuskalmetWeatherStationFromXMLFile(String inputFileString){
 		log.debug("createEuskalmetWeatherStationFromXMLFile START");
@@ -90,7 +134,7 @@ public class EuskalmetWeatherStationExtractor {
 					EuskalmetWeatherStation.getEuskalmetStation_Id());
 			log.debug("Station ID: " + ws.stationID);
 			
-			ws.stationURI = baseURI + weatherStationBase + ws.stationID;
+			ws.stationURI = baseURI + euskalmetWeatherStationBase + ws.stationID;
 			log.debug("Station URI: " + ws.stationURI);
 			
 			if (root.getElementsByTagName(EuskalmetWeatherStation.getEuskalmetStation_Name()).getLength() > 0){
@@ -99,7 +143,7 @@ public class EuskalmetWeatherStationExtractor {
 				log.debug("Station Name: " + ws.stationName);
 			}
 			
-			ws.stationOwnerURI = weatherStationOwnerURI;
+			ws.stationOwnerURI = euskalmetWeatherStationOwnerURI;
 			log.debug("Station Owner: " + ws.stationOwnerURI);
 			
 			
@@ -161,6 +205,11 @@ public class EuskalmetWeatherStationExtractor {
 								xmlSensorElement.getAttribute(EuskalmetWeatherStation.getEuskalmetStation_Sensor_Id());
 						log.debug("Sensor ID: " + sensorList[sensorIterator].sensorId);
 						
+						String obsQuality = getObservedQualityURIFromEuskalmetSensorId(sensorList[sensorIterator].sensorId);
+						
+						sensorList[sensorIterator].observedQuality.qualityName = obsQuality.substring(obsQuality.lastIndexOf("#")+1);
+						sensorList[sensorIterator].observedQuality.qualityURI = obsQuality;
+						
 						sensorList[sensorIterator].sensorURI = ws.stationURI + "_" + sensorList[sensorIterator].sensorId;
 						log.debug("Sensor URI: " + sensorList[sensorIterator].sensorURI);
 						
@@ -182,13 +231,13 @@ public class EuskalmetWeatherStationExtractor {
 						
 						if (xmlSensorElement.getElementsByTagName(
 								EuskalmetWeatherStation.getEuskalmetStation_Sensor_Alt()).getLength() > 0){
-							sensorList[sensorIterator].sensorAlt = 
-									xmlSensorElement.getElementsByTagName(
-											EuskalmetWeatherStation.getEuskalmetStation_Sensor_Alt()).item(0).getTextContent();
+							float sensorAlt = 
+									Float.parseFloat(xmlSensorElement.getElementsByTagName(
+											EuskalmetWeatherStation.getEuskalmetStation_Sensor_Alt()).item(0).getTextContent());
+							
+							sensorList[sensorIterator].sensorAlt = Float.toString(sensorAlt/100); //In meters
 							log.debug("Sensor Altitude: " + sensorList[sensorIterator].sensorAlt);
 						}
-						
-							
 						
 					}
 					else{
@@ -209,6 +258,61 @@ public class EuskalmetWeatherStationExtractor {
 		
 		log.debug("createEuskalmetWeatherStationFromXML END");
 		return ws;
+	}
+
+	public static String[] getEuskalmetWeatherStationURLList(String stationListURLXML){
+		log.debug("getEuskalmetWeatherStationURLList START");
+		
+		if(stationListURLXML.isEmpty()){
+			log.error("ERROR: in getEuskalmetWeatherStationURLList");
+			log.error("ERROR DESC: getEuskalmetWeatherStationURLList " + stationListURLXML + " IS EMPTY");
+		}
+		
+		String[] URLList = null;
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		
+		Document doc = null;
+		try {
+			doc = factory.newDocumentBuilder().parse(new URL(stationListURLXML).openStream());
+			
+			int wsURLListLength = 0;
+			
+			//get <row>
+			NodeList wsURLList = doc.getElementsByTagName("row");
+			wsURLListLength = wsURLList.getLength();
+			
+			URLList = new String[wsURLListLength];
+			
+			for (int wsURLIterator=0; wsURLIterator<wsURLListLength; wsURLIterator++) 
+		    {
+				Element element = (Element)wsURLList.item(wsURLIterator);	       
+				String attrDataXML = new String();
+				
+				if (element.getElementsByTagName("dataxml").getLength() > 0)
+					attrDataXML = element.getElementsByTagName("dataxml").item(0).getTextContent();
+				
+		        if (!attrDataXML.isEmpty())
+		        	URLList[wsURLIterator] = attrDataXML;  
+		    }
+		} catch (MalformedURLException e) {
+			log.error("ERROR: in getEuskalmetWeatherStationURLList");
+			log.error("ERROR DESC: MalformedURLException", e);
+		} catch (SAXException e) {
+			log.error("ERROR: in getEuskalmetWeatherStationURLList");
+			log.error("ERROR DESC: SAXException", e);
+		} catch (IOException e) {
+			log.error("ERROR: in getEuskalmetWeatherStationURLList");
+			log.error("ERROR DESC: IOException", e);
+		} catch (ParserConfigurationException e) {
+			log.error("ERROR: in getEuskalmetWeatherStationURLList");
+			log.error("ERROR DESC: ParserConfigurationException", e);
+		}
+		
+		log.debug("getEuskalmetWeatherStationURLList END");
+		return URLList;
+		
 	}
 	
 	private static String getProvinceURI(String provinceString) {
@@ -265,6 +369,155 @@ public class EuskalmetWeatherStationExtractor {
 		return coord;
 	}
 	
-	
+	private static String getObservedQualityURIFromEuskalmetSensorId(String sensorId) {
+		/*
+		 * This function gets the URI of the quality observed by a Euskalmet Sensor 
+		 */
+		log.debug("getObservedQualityURIFromEuskalmetSensorId: START");
+		
+		String observedQualityURI = Namespaces.getNs_Q4EEPSA() + "UnknownQuality";
+		
+		if (sensorId.equals("11"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WindSpeed";
+			
+		else if (sensorId.equals("12"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WindDirection";
+			
+		else if (sensorId.equals("14"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "MaxWindSpeed"; //
+			
+		else if (sensorId.equals("16"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WindSpeedSigma"; //Sigma dirección del viento
+			
+		else if (sensorId.equals("17"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SigmaTheta"; //Sigma velocidad del viento
+			
+		else if (sensorId.equals("18"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WindPower"; //Velocidad del viento cúbica media  
+			
+		else if (sensorId.equals("21"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "OutdoorTemperature";
+
+		else if (sensorId.equals("22"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "GroundLevelTemperature";
+		
+		else if (sensorId.equals("31"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "OutdoorHumidity";
+			
+		else if (sensorId.equals("40"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "PrecipitationLevel";
+			
+		else if (sensorId.equals("50"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "AtmosphericPressure";
+						
+		else if (sensorId.equals("60"))
+			observedQualityURI = Namespaces.getNs_M3LITE() + "WaterLevel";
+						
+		else if (sensorId.equals("61"))
+			observedQualityURI = Namespaces.getNs_M3LITE() + "WaterLevel"; 
+						
+		else if (sensorId.equals("62"))
+			observedQualityURI = Namespaces.getNs_M3LITE() + "WaterLevel"; 
+						
+		else if (sensorId.equals("65"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaterFlow"; 
+						
+		else if (sensorId.equals("70"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SolarRadiation";
+		
+		else if (sensorId.equals("74"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "NetRadiation";
+						
+		else if (sensorId.equals("90"))
+			observedQualityURI = Namespaces.getNs_M3LITE() + "WaterTemperature";
+						
+		else if (sensorId.equals("91"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "DissolvedOxygen";
+						
+		else if (sensorId.equals("92"))
+			observedQualityURI = Namespaces.getNs_M3LITE() + "PH";
+						
+		else if (sensorId.equals("93"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaterConductivity";
+						
+		else if (sensorId.equals("94"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "AmmoniaInWater";
+						
+		else if (sensorId.equals("95"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaterTurbidity";
+						
+		else if (sensorId.equals("97"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "OrganicCompoundsInWater";
+						
+		else if (sensorId.equals("98"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "NitrateInWater";
+		
+		else if (sensorId.equals("B0"))
+			observedQualityURI = Namespaces.getNs_M3LITE() + "Visibility";
+		
+		else if (sensorId.equals("B1"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SeaWaterLevel";
+		
+		else if (sensorId.equals("B2"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SeaWaterLevel";
+
+		else if (sensorId.equals("B4"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "MaxWaveHeight";
+		
+		else if (sensorId.equals("B5"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaveHeight";
+
+		else if (sensorId.equals("B6"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SwellPeriod";
+		
+		else if (sensorId.equals("B7"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "HydrostaticPressureMSP";
+		
+		else if (sensorId.equals("B8"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "HydrostaticPressureLSP";
+		
+		else if (sensorId.equals("B9"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaterFlowMagnitude";
+		
+		else if (sensorId.equals("BA"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaterFlowDirection";
+		
+		else if (sensorId.equals("BB"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SeaWaterTemperature";
+		
+		else if (sensorId.equals("BC"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "TermistorTemperature";
+		
+		else if (sensorId.equals("BD"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaveHeight";
+
+		else if (sensorId.equals("BE"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "WaveFlowVerticalSpeed";
+		
+		else if (sensorId.equals("BF"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "TideLevel";
+
+		else if (sensorId.equals("BG"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "PeakPeriod";
+		
+		else if (sensorId.equals("BH"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "PeakPeriodDirection";
+		
+		else if (sensorId.equals("BI"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SwellAverageDirection";
+		
+		else if (sensorId.equals("FT"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "Phosphorus";
+		
+		else if (sensorId.equals("OR"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "OrtoPhosphate";
+
+		else if (sensorId.equals("SS"))
+			observedQualityURI = Namespaces.getNs_Q4EEPSA() + "SuspendedSolids";
+
+		log.debug("Observed Quality: " + observedQualityURI);
+		log.debug("getObservedQualityURIFromEuskalmetSensorId: END");
+		return observedQualityURI;
+	}
 	
 }
