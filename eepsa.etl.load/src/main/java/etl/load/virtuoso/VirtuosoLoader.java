51be807d77a686b998651a479d0168bfa6da9131
Iker Esnaola <iker.esnaola@tekniker.es>
2019-02-21 10:38:53 +0100

    Code updated and fixed

--- eepsa.etl.load/src/main/java/etl/load/virtuoso/VirtuosoLoader.java
+++ eepsa.etl.load/src/main/java/etl/load/virtuoso/VirtuosoLoader.java
@@ -2,37 +2,49 @@ package etl.load.virtuoso;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdfconnection.RDFConnection;
-import org.apache.jena.rdfconnection.RDFConnectionFactory;
-import org.apache.jena.update.UpdateAction;
+import com.hp.hpl.jena.rdf.model.Model;
+
+import virtuoso.jena.driver.VirtModel;
+
 
 public class VirtuosoLoader {
 
-	
 	private static Log log = LogFactory.getLog(VirtuosoLoader.class);
 	
-	public static Model updateModel(String rdfFilename, String sparqlQuery){
-		log.debug("updateModel: START");
-		Model model = ModelFactory.createDefaultModel() ;
-		model.read(rdfFilename) ;
-		UpdateAction.parseExecute(sparqlQuery, model);
-		
-		log.debug("updateModel: END");
-		return model;
-	}
 	
-	public static int loadModelToVirtuoso (Model model, String sparqlEndpointURL, String targetGraph, String sparqlUpdateQuery){
-		log.debug("loadModelToVirtuoso: START");
-		int correctlyLoaded = 0;
-		
-		try (RDFConnection conn = RDFConnectionFactory.connect(sparqlEndpointURL)) {
-		        conn.load(targetGraph, model);
-		        correctlyLoaded = 1;
+	public static int loadModelToVirtuoso(Model model, String endpoint, String GRAPH_NAME, String USERNAME, String PASSWORD){
+	/**
+	 * This function loads a Model to Virtuoso	
+	 */
+		log.debug("loadModelToVirtuoso START");
+		try {
+			//Connect to Virtuoso
+			VirtModel virtualModel = VirtModel.openDatabaseModel(GRAPH_NAME, endpoint, USERNAME, PASSWORD);
+			long modelInitialSize = virtualModel.size();
+			
+			//Add model
+			virtualModel.add(model);
+			long modelNewSize = virtualModel.size();
+			
+			virtualModel.close();
+			
+			//Assuming that Virtuoso was empty (or at least it does not have this info yet)
+			if (modelInitialSize < modelNewSize){
+				log.debug("RDF content added.");
+				log.debug("loadModelToVirtuoso END");
+				return 1;
+			}
+				
+			else{
+				log.debug("RDF content NOT added.");
+				log.debug("loadModelToVirtuoso END");
+				return 0;
+			}
 		}
-		log.debug("loadModelToVirtuoso: END");
-		return correctlyLoaded;
+		catch (Exception e){
+			log.error("ERROR: " + e);
+			log.debug("loadModelToVirtuoso END");
+			return 0;
+		}
 	}
-	
 }
